fc0fbc3f9cb51dd83deeb29e93229a94c5b6e434
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2023-07-15 20:22:38 +0600

    Update UploadRequest.jsx

--- client/src/Api/UploadRequest.jsx
+++ client/src/Api/UploadRequest.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:8080' })
+let baseUrl = process.env.REACT_APP_PUBLIC_FOLDER
+baseUrl = baseUrl.slice(0, baseUrl.length - 8)
+
+const API = axios.create({ baseURL: baseUrl })
 
 export const uploadImage = (data) => API.post('/upload/', data)
 
