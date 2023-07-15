846aad34f363d2b5c72caa31c581f151bd740948
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2023-07-15 21:28:51 +0600

    Update User.jsx

--- client/src/components/user/User.jsx
+++ client/src/components/user/User.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from 'react'
+import './User.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { followUser, unFollowUser } from '../../Actions/userActions.jsx'
+import { getUser } from '../../Api/UserRequest.jsx'
 
 const User = ({ person }) => {
   const { user } = useSelector((state) => state.AuthReducer.authData)
+  const [data, setData] = useState({})
+
+  const handleData = async (id) => {
+    if (Object.keys(data).length === 0) {
+      let newData = await getUser(id)
+      setData(newData?.data)
+    } else {
+      setData({})
+    }
+  }
+  console.log(data)
+
   const dispatch = useDispatch()
+
   const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER
 
   const [following, setFollowing] = useState(
@@ -12,7 +27,7 @@ const User = ({ person }) => {
   )
 
   const handleFollow = () => {
-     if (following) {
+    if (following) {
       dispatch(unFollowUser(person._id, user._id))
       alert('UnFollowed user')
     } else {
@@ -24,38 +39,51 @@ const User = ({ person }) => {
   }
   // console.log(following)
   return (
-    <div className="follower">
-      <div>
-        <img
-          src={
-            person.profilePicture !== undefined || null
-              ? publicFolder + person.profilePicture
-              : `${publicFolder}defaultProfile.png`
-          }
-          alt="profile"
-          className="followerImg"
-        />
-        <div className="name">
-          <span>
-            {person.firstName} {person.lastName}
-          </span>
-          <span>
-            @
-            {person?.username.length > 18
-              ? `${person?.username.substr(0, 16)}...`
-              : person?.username}
-          </span>
+    <>
+      <div className="follower">
+        <div onClick={() => handleData(person._id)}>
+          <img
+            src={
+              person.profilePicture !== undefined || null
+                ? publicFolder + person.profilePicture
+                : `${publicFolder}defaultProfile.png`
+            }
+            alt="profile"
+            className="followerImg"
+          />
+          <div className="name">
+            <span>
+              {person.firstName} {person.lastName}
+            </span>
+            <span>
+              @
+              {person?.username.length > 18
+                ? `${person?.username.substr(0, 16)}...`
+                : person?.username}
+            </span>
+          </div>
         </div>
+        <button
+          className={
+            following ? 'button fc-button UnFollowButton' : 'button fc-button'
+          }
+          onClick={handleFollow}
+        >
+          {following ? 'UnFollow' : 'Follow'}
+        </button>
       </div>
-      <button
-        className={
-          following ? 'button fc-button UnFollowButton' : 'button fc-button'
-        }
-        onClick={handleFollow}
-      >
-        {following ? 'UnFollow' : 'Follow'}
-      </button>
-    </div>
+      {Object.keys(data).length !== 0 ? (
+        <div className="selectedUser">
+          <div>{data?.username}</div>
+          <div className="line"></div>
+          <div>{data?.followers?.length}</div>
+          <div className="line"></div>
+          <div>{data?.following?.length}</div>
+        </div>
+      ) : (
+        ''
+      )}
+    </>
   )
 }
 
